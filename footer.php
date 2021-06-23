cbe5d30120c1dd773668d30be01177da1cd0dcc1
yanagawa <yanagawa0328@gmail.com>
2021-06-23 16:06:58 +0900

    柳川アップロード

---
+++ footer.php
@@ -0,0 +1,10 @@
+<footer>
+  <div class="display">
+    <h1 class="banner">G's SNS</h1>
+    <ul class="sub">
+      <li>利用規約</li>
+      <li>プライバシーポリシー</li>
+      <li>お問い合わせ</li>
+    </ul>
+  </div>
+</footer>
