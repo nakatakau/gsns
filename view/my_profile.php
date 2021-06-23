28bb624835801e46da76943bfad7355a403b288a
nakamura takuma <takuma.nakamura1131@gmail.com>
2021-06-23 16:35:21 +0900

    my_profile.phpを追加

--- view/my_profile.php
+++ view/my_profile.php
@@ -13,112 +13,143 @@
 </head>
 
 <body>
-  <!-- プロフィールの１番目のブロック（アイコンとメッセージ） -->
-  <div class="top">
-    <div class="icon">
-      <img src="" alt="" class="icon_img">
-      <div class="icon_name_area">
-        <p class="name">高橋ひろき（31歳）</p>
-        <p class="gs_year">LAB 11期</p>
+  <div class="button_area">
+    <button class="edit_button">編集する</button>
+  </div>
+  <div class="profile_flex">
+    <!-- プロフィールの１番目のブロック（アイコンとメッセージ） -->
+    <div class="left">
+      <div class="icon">
+        <p class="gs_year">東京 LAB 11期</p>
+        <p class="graduate">2021年10月卒業予定</p>
+        <div class="icon_area">
+          <img src="" alt="" class="icon_img">
+        </div>
+        <div class="icon_name_area">
+          <p class="hurigana">かくまあり</p>
+          <p class="name">賀来真有</p>
+          <p class="gender">女性</p>
+          <p class="birth">1996/11/15（24歳）</p>
+        </div>
+      </div>
+      <div class="message_area">
+        ここに一言
+      </div>
+      <div class="other">
+        <table class="other_table">
+          <tr class="cell">
+            <td class="cell_left">血液型</td>
+            <td class="cell_right">A型</td>
+          </tr>
+          <tr class="cell">
+            <td class="cell_left">居住地</td>
+            <td class="cell_right">東京</td>
+          </tr>
+          <tr class="cell">
+            <td class="cell_left">出身地</td>
+            <td class="cell_right">東京</td>
+          </tr>
+          <tr class="cell">
+            <td class="cell_left">年収</td>
+            <td class="cell_right">700-1000万</td>
+          </tr>
+          <tr class="cell">
+            <td class="cell_left">英語スキル</td>
+            <td class="cell_right">ビジネスレベル</td>
+          </tr>
+          <tr class="cell">
+            <td class="cell_left">性格</td>
+            <td class="cell_right">穏やか</td>
+          </tr>
+        </table>
       </div>
     </div>
-    <div class="message_area">
-      ここにメッセージ
+    <div class="right">
+      <!-- なぜG'sに入学したか? -->
+      <div class="profile_area">
+        <div class="profile_area_title">
+          <p class="profile_area_name"> なぜG'sに入学したか? </p>
+        </div>
+        <div class="profile_area_content">
+        </div>
+      </div>
+      <!-- プロフィールの２番目のブロック（ポートフォリオ） -->
+      <div class="second">
+        <p class="portfolio_title">ポートフォリオ・作品</p>
+        <!-- １つ目のポートフォリオ -->
+        <div class="portfolio_area" id="portfolio_area_1">
+          <p class="portfolio_area_title" id="portfolio_area_title1">深堀チャットアプリ</p>
+          <p class="portfolio_area_description" id="portfolio_area_description1">説明</p>
+          <div class="portfolio_area_content" id="portfolio_area_content1"></div>
+        </div>
+        <!-- ２つ目のポートフォリオ -->
+        <div class="portfolio_area" id="portfolio_area_2">
+          <p class="portfolio_area_title" id="portfolio_area_title2">深堀チャットアプリ</p>
+          <p class="portfolio_area_description" id="portfolio_area_description2">説明</p>
+          <div class="portfolio_area_content" id="portfolio_area_content2"></div>
+        </div>
+        <!-- ３つ目のポートフォリオ -->
+        <div class="portfolio_area" id="portfolio_area_3">
+          <p class="portfolio_area_title" id="portfolio_area_title3">深堀チャットアプリ</p>
+          <p class="portfolio_area_description" id="portfolio_area_description3">説明</p>
+          <div class="portfolio_area_content" id="portfolio_area_content3"></div>
+        </div>
+      </div>
+      <!-- プロフィールの３番目のブロック（スキル・経歴） -->
+      <div class="third">
+        <!-- 職種 -->
+        <div class="profile_area">
+          <div class="profile_area_title">
+            <p class="profile_area_name"> 現在の職種（３個まで選択可能） </p>
+          </div>
+          <div class="profile_area_content2">
+          </div>
+        </div>
+        <!-- 使用可能プログラミング言語 -->
+        <div class="profile_area">
+          <div class="profile_area_title">
+            <p class="profile_area_name"> 使用可能プログラミング言語（6個まで選択可能） </p>
+          </div>
+          <div class="profile_area_content2">
+          </div>
+        </div>
+        <!--使用可能アプリケーション -->
+        <div class="profile_area">
+          <div class="profile_area_title">
+            <p class="profile_area_name"> 使用可能アプリケーション（6個まで選択可能） </p>
+          </div>
+          <div class="profile_area_content2">
+          </div>
+        </div>
+        <!-- 来歴（学歴・職歴など） -->
+        <div class="profile_area">
+          <div class="profile_area_title">
+            <p class="profile_area_name"> 来歴（学歴・職歴など） </p>
+          </div>
+          <div class="profile_area_content">
+          </div>
+        </div>
+        <!--  資格 -->
+        <div class="profile_area">
+          <div class="profile_area_title">
+            <p class="profile_area_name"> 資格 </p>
+          </div>
+          <div class="profile_area_content">
+          </div>
+        </div>
+        <!-- フリーコメント -->
+        <div class="profile_area">
+          <div class="profile_area_title">
+            <p class="profile_area_name"> フリーコメント </p>
+          </div>
+          <div class="profile_area_content">
+          </div>
+        </div>
+      </div>
     </div>
   </div>
-  <!-- プロフィールの２番目のブロック（ポートフォリオ） -->
-  <div class="second">
-    <p class="portfolio_title">ポートフォリオ・作品</p>
-    <!-- １つ目のポートフォリオ -->
-    <div class="portfolio_area" id="portfolio_area_1">
-      <p class="portfolio_area_title" id="portfolio_area_title1">深堀チャットアプリ</p>
-      <div class="portfolio_area_content" id="portfolio_area_content1"></div>
-    </div>
-    <!-- ２つ目のポートフォリオ -->
-    <div class="portfolio_area" id="portfolio_area_2">
-      <p class="portfolio_area_title" id="portfolio_area_title2">深堀チャットアプリ</p>
-      <div class="portfolio_area_content" id="portfolio_area_content2"></div>
-    </div>
-    <!-- ３つ目のポートフォリオ -->
-    <div class="portfolio_area" id="portfolio_area_3">
-      <p class="portfolio_area_title" id="portfolio_area_title3">深堀チャットアプリ</p>
-      <div class="portfolio_area_content" id="portfolio_area_content3"></div>
-    </div>
-  </div>
-  <!-- プロフィールの３番目のブロック（スキル・経歴） -->
-  <div class="third">
-    <!-- スキル・特徴 -->
-    <div class="profile_area">
-      <div class="profile_area_title">
-        <p class="profile_area_name"> スキル特徴 </p>
-        <button class="edit_button">編集</button>
-      </div>
-      <div class="profile_area_content">
-      </div>
-    </div>
-    <!-- Why me -->
-    <div class="profile_area">
-      <div class="profile_area_title">
-        <p class="profile_area_name"> Why me? </p>
-        <button class="edit_button">編集</button>
-      </div>
-      <div class="profile_area_content">
-      </div>
-    </div>
-    <!-- Who What -->
-    <div class="profile_area">
-      <div class="profile_area_title">
-        <p class="profile_area_name"> Who What? </p>
-        <button class="edit_button">編集</button>
-      </div>
-      <div class="profile_area_content">
-      </div>
-    </div>
-    <!-- How -->
-    <div class="profile_area">
-      <div class="profile_area_title">
-        <p class="profile_area_name"> How? </p>
-        <button class="edit_button">編集</button>
-      </div>
-      <div class="profile_area_content">
-      </div>
-    </div>
-    <!-- 困っていること -->
-    <div class="profile_area">
-      <div class="profile_area_title">
-        <p class="profile_area_name"> 困っていること </p>
-        <button class="edit_button">編集</button>
-      </div>
-      <div class="profile_area_content">
-      </div>
-    </div>
-    <!-- 職歴 -->
-    <div class="profile_area">
-      <div class="profile_area_title">
-        <p class="profile_area_name"> 職歴 </p>
-        <button class="edit_button">編集</button>
-      </div>
-      <div class="profile_area_content">
-      </div>
-    </div>
-    <!-- 学歴 -->
-    <div class="profile_area">
-      <div class="profile_area_title">
-        <p class="profile_area_name"> 学歴 </p>
-        <button class="edit_button">編集</button>
-      </div>
-      <div class="profile_area_content">
-      </div>
-    </div>
-    <!-- フリーコメント -->
-    <div class="profile_area">
-      <div class="profile_area_title">
-        <p class="profile_area_name"> フリーコメント </p>
-        <button class="edit_button">編集</button>
-      </div>
-      <div class="profile_area_content">
-      </div>
-    </div>
+  <div class="button_area">
+    <button class="edit_button">保存する</button>
   </div>
 </body>
 
