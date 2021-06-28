9433a335d12bd76b2f5d97421287b57b09c99824
nakamura takuma <takuma.nakamura1131@gmail.com>
2021-06-28 13:30:57 +0900

    一旦、indexを提出

--- view/index.php
+++ view/index.php
@@ -22,7 +22,7 @@ $available_programming_language_id_count = count($available_programming_language
 // DB接続
 $pdo = db_conn();
 // 職種選択ボックスで何かしらあれば、職種を持つユーザーIDを取得する処理
-$occupations=[];
+$occupations = [];
 if ($occupation_count !== 0) {
 
   $where_occupations = '';
@@ -91,17 +91,17 @@ if ($available_programming_language_id_count !== 0) {
 }
 
 // 職種と利用可能言語を持つユーザidを結合
-$search_target_users_id=array_merge($occupations,$langs);
+$search_target_users_id = array_merge($occupations, $langs);
 // v($search_target_users_id);
 // 結合した配列から重複を排除
 $search_target_users_id = array_unique($search_target_users_id);
 v($search_target_users_id);
-$search_target_users_id_count =count($search_target_users_id);
+$search_target_users_id_count = count($search_target_users_id);
 // v($search_target_users_id_count);
 
 // 職種と利用可能言語を持つユーザーが一人以上なら検索処理(usersテーブルから基本情報を取得する処理)
-if ($search_target_users_id_count>=1) {
-  $where_users_id='';
+if ($search_target_users_id_count >= 1) {
+  $where_users_id = '';
   foreach ($search_target_users_id as $user_id) {
     $where_users_id .= "user_id = " . $user_id . " or ";
   }
@@ -464,7 +464,7 @@ if ($search_target_users_id_count>=1) {
 
           <!-- -----その他の選択----- -->
 
-          <h1 class="#">その他</h1>
+          <!-- <h1 class="#">その他</h1>
           <div class="sonota_sentaku">
 
             <div class="check_box_design sonota">
@@ -481,10 +481,10 @@ if ($search_target_users_id_count>=1) {
               <input type="checkbox" class="sotugyou_sentaku" id="sotugyou_sentaku1" name="admission_period[]" value="">
               <label for="sotugyou_sentaku1">卒業生のみ</label>
             </div>
-          </div>
+          </div> -->
 
           <!-- -----ステータスの選択----- -->
-          <div class="kihon_info">
+          <!-- <div class="kihon_info">
 
             <div class="resident">
               <label for="resident">居住地</label>
@@ -584,8 +584,7 @@ if ($search_target_users_id_count>=1) {
                 <option value="2">その他</option>
               </select>
             </div>
-
-          </div>
+          </div> -->
           <!-- -----検索ボタン----- -->
           <input type="submit" id="btn" class="btn" value="検索">
         </div>
@@ -753,217 +752,236 @@ if ($search_target_users_id_count>=1) {
   <script>
     //featherアイコン
     feather.replace();
-
-    // ----------------------------------------------
-    // 都道府県のオプションを追加
-    // ----------------------------------------------
-    let array = [{
-      code: '1',
-      name: '北海道',
-      en: 'Hokkaidô'
-    }, {
-      code: '2',
-      name: '青森県',
-      en: 'Aomori'
-    }, {
-      code: '3',
-      name: '岩手県',
-      en: 'Iwate'
-    }, {
-      code: '4',
-      name: '宮城県',
-      en: 'Miyagi'
-    }, {
-      code: '5',
-      name: '秋田県',
-      en: 'Akita'
-    }, {
-      code: '6',
-      name: '山形県',
-      en: 'Yamagata'
-    }, {
-      code: '7',
-      name: '福島県',
-      en: 'Hukusima'
-    }, {
-      code: '8',
-      name: '茨城県',
-      en: 'Ibaraki'
-    }, {
-      code: '9',
-      name: '栃木県',
-      en: 'Totigi'
-    }, {
-      code: '10',
-      name: '群馬県',
-      en: 'Gunma'
-    }, {
-      code: '11',
-      name: '埼玉県',
-      en: 'Saitama'
-    }, {
-      code: '12',
-      name: '千葉県',
-      en: 'Tiba'
-    }, {
-      code: '13',
-      name: '東京都',
-      en: 'Tôkyô'
-    }, {
-      code: '14',
-      name: '神奈川県',
-      en: 'Kanagawa'
-    }, {
-      code: '15',
-      name: '新潟県',
-      en: 'Niigata'
-    }, {
-      code: '16',
-      name: '富山県',
-      en: 'Toyama'
-    }, {
-      code: '17',
-      name: '石川県',
-      en: 'Isikawa'
-    }, {
-      code: '18',
-      name: '福井県',
-      en: 'Hukui'
-    }, {
-      code: '19',
-      name: '山梨県',
-      en: 'Yamanasi'
-    }, {
-      code: '20',
-      name: '長野県',
-      en: 'Nagano'
-    }, {
-      code: '21',
-      name: '岐阜県',
-      en: 'Gihu'
-    }, {
-      code: '22',
-      name: '静岡県',
-      en: 'Sizuoka'
-    }, {
-      code: '23',
-      name: '愛知県',
-      en: 'Aiti'
-    }, {
-      code: '24',
-      name: '三重県',
-      en: 'Mie'
-    }, {
-      code: '25',
-      name: '滋賀県',
-      en: 'Siga'
-    }, {
-      code: '26',
-      name: '京都府',
-      en: 'Kyôto'
-    }, {
-      code: '27',
-      name: '大阪府',
-      en: 'Ôsaka'
-    }, {
-      code: '28',
-      name: '兵庫県',
-      en: 'Hyôgo'
-    }, {
-      code: '29',
-      name: '奈良県',
-      en: 'Nara'
-    }, {
-      code: '30',
-      name: '和歌山県',
-      en: 'Wakayama'
-    }, {
-      code: '31',
-      name: '鳥取県',
-      en: 'Tottori'
-    }, {
-      code: '32',
-      name: '島根県',
-      en: 'Simane'
-    }, {
-      code: '33',
-      name: '岡山県',
-      en: 'Okayama'
-    }, {
-      code: '34',
-      name: '広島県',
-      en: 'Hirosima'
-    }, {
-      code: '35',
-      name: '山口県',
-      en: 'Yamaguti'
-    }, {
-      code: '36',
-      name: '徳島県',
-      en: 'Tokusima'
-    }, {
-      code: '37',
-      name: '香川県',
-      en: 'Kagawa'
-    }, {
-      code: '38',
-      name: '愛媛県',
-      en: 'Ehime'
-    }, {
-      code: '39',
-      name: '高知県',
-      en: 'Kôti'
-    }, {
-      code: '40',
-      name: '福岡県',
-      en: 'Hukuoka'
-    }, {
-      code: '41',
-      name: '佐賀県',
-      en: 'Saga'
-    }, {
-      code: '42',
-      name: '長崎県',
-      en: 'Nagasaki'
-    }, {
-      code: '43',
-      name: '熊本県',
-      en: 'Kumamoto'
-    }, {
-      code: '44',
-      name: '大分県',
-      en: 'Ôita'
-    }, {
-      code: '45',
-      name: '宮崎県',
-      en: 'Miyazaki'
-    }, {
-      code: '46',
-      name: '鹿児島県',
-      en: 'Kagosima'
-    }, {
-      code: '47',
-      name: '沖縄県',
-      en: 'Kagosima'
-    }, {
-      code: '49',
-      name: 'その他',
-      en: ''
-    }, ];
-    const address = document.getElementById('address');
-    const from = document.getElementById('from');
-    array.forEach(target => {
-      const option = document.createElement('option');
-      option.value = target.code;
-      option.textContent = target.name;
-      address.appendChild(option);
-    })
-    array.forEach(target => {
-      const option = document.createElement('option');
-      option.value = target.code;
-      option.textContent = target.name;
-      from.appendChild(option);
-    })
+    // // ----------------------------------------------
+    // // 都道府県のオプションを追加
+    // // ----------------------------------------------
+    // let array = [{
+    //   code: '1',
+    //   name: '北海道',
+    //   en: 'Hokkaidô'
+    // }, {
+    //   code: '2',
+    //   name: '青森県',
+    //   en: 'Aomori'
+    // }, {
+    //   code: '3',
+    //   name: '岩手県',
+    //   en: 'Iwate'
+    // }, {
+    //   code: '4',
+    //   name: '宮城県',
+    //   en: 'Miyagi'
+    // }, {
+    //   code: '5',
+    //   name: '秋田県',
+    //   en: 'Akita'
+    // }, {
+    //   code: '6',
+    //   name: '山形県',
+    //   en: 'Yamagata'
+    // }, {
+    //   code: '7',
+    //   name: '福島県',
+    //   en: 'Hukusima'
+    // }, {
+    //   code: '8',
+    //   name: '茨城県',
+    //   en: 'Ibaraki'
+    // }, {
+    //   code: '9',
+    //   name: '栃木県',
+    //   en: 'Totigi'
+    // }, {
+    //   code: '10',
+    //   name: '群馬県',
+    //   en: 'Gunma'
+    // }, {
+    //   code: '11',
+    //   name: '埼玉県',
+    //   en: 'Saitama'
+    // }, {
+    //   code: '12',
+    //   name: '千葉県',
+    //   en: 'Tiba'
+    // }, {
+    //   code: '13',
+    //   name: '東京都',
+    //   en: 'Tôkyô'
+    // }, {
+    //   code: '14',
+    //   name: '神奈川県',
+    //   en: 'Kanagawa'
+    // }, {
+    //   code: '15',
+    //   name: '新潟県',
+    //   en: 'Niigata'
+    // }, {
+    //   code: '16',
+    //   name: '富山県',
+    //   en: 'Toyama'
+    // }, {
+    //   code: '17',
+    //   name: '石川県',
+    //   en: 'Isikawa'
+    // }, {
+    //   code: '18',
+    //   name: '福井県',
+    //   en: 'Hukui'
+    // }, {
+    //   code: '19',
+    //   name: '山梨県',
+    //   en: 'Yamanasi'
+    // }, {
+    //   code: '20',
+    //   name: '長野県',
+    //   en: 'Nagano'
+    // }, {
+    //   code: '21',
+    //   name: '岐阜県',
+    //   en: 'Gihu'
+    // }, {
+    //   code: '22',
+    //   name: '静岡県',
+    //   en: 'Sizuoka'
+    // }, {
+    //   code: '23',
+    //   name: '愛知県',
+    //   en: 'Aiti'
+    // }, {
+    //   code: '24',
+    //   name: '三重県',
+    //   en: 'Mie'
+    // }, {
+    //   code: '25',
+    //   name: '滋賀県',
+    //   en: 'Siga'
+    // }, {
+    //   code: '26',
+    //   name: '京都府',
+    //   en: 'Kyôto'
+    // }, {
+    //   code: '27',
+    //   name: '大阪府',
+    //   en: 'Ôsaka'
+    // }, {
+    //   code: '28',
+    //   name: '兵庫県',
+    //   en: 'Hyôgo'
+    // }, {
+    //   code: '29',
+    //   name: '奈良県',
+    //   en: 'Nara'
+    // }, {
+    //   code: '30',
+    //   name: '和歌山県',
+    //   en: 'Wakayama'
+    // }, {
+    //   code: '31',
+    //   name: '鳥取県',
+    //   en: 'Tottori'
+    // }, {
+    //   code: '32',
+    //   name: '島根県',
+    //   en: 'Simane'
+    // }, {
+    //   code: '33',
+    //   name: '岡山県',
+    //   en: 'Okayama'
+    // }, {
+    //   code: '34',
+    //   name: '広島県',
+    //   en: 'Hirosima'
+    // }, {
+    //   code: '35',
+    //   name: '山口県',
+    //   en: 'Yamaguti'
+    // }, {
+    //   code: '36',
+    //   name: '徳島県',
+    //   en: 'Tokusima'
+    // }, {
+    //   code: '37',
+    //   name: '香川県',
+    //   en: 'Kagawa'
+    // }, {
+    //   code: '38',
+    //   name: '愛媛県',
+    //   en: 'Ehime'
+    // }, {
+    //   code: '39',
+    //   name: '高知県',
+    //   en: 'Kôti'
+    // }, {
+    //   code: '40',
+    //   name: '福岡県',
+    //   en: 'Hukuoka'
+    // }, {
+    //   code: '41',
+    //   name: '佐賀県',
+    //   en: 'Saga'
+    // }, {
+    //   code: '42',
+    //   name: '長崎県',
+    //   en: 'Nagasaki'
+    // }, {
+    //   code: '43',
+    //   name: '熊本県',
+    //   en: 'Kumamoto'
+    // }, {
+    //   code: '44',
+    //   name: '大分県',
+    //   en: 'Ôita'
+    // }, {
+    //   code: '45',
+    //   name: '宮崎県',
+    //   en: 'Miyazaki'
+    // }, {
+    //   code: '46',
+    //   name: '鹿児島県',
+    //   en: 'Kagosima'
+    // }, {
+    //   code: '47',
+    //   name: '沖縄県',
+    //   en: 'Kagosima'
+    // }, {
+    //   code: '49',
+    //   name: 'その他',
+    //   en: ''
+    // }, ];
+    // const address = document.getElementById('address');
+    // const from = document.getElementById('from');
+    // array.forEach(target => {
+    //   const option = document.createElement('option');
+    //   option.value = target.code;
+    //   option.textContent = target.name;
+    //   address.appendChild(option);
+    // })
+    // array.forEach(target => {
+    //   const option = document.createElement('option');
+    //   option.value = target.code;
+    //   option.textContent = target.name;
+    //   from.appendChild(option);
+    // })
+    // ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+    // プロフィールカードのDOM実装
+    // ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+    // プロフィールエリアの取得
+    const card_two = document.getElementById('card_two');
+    // プロフィールデータの取得
+    const JSON_user = <?= $result_json_users; ?>;
+    const JSON_user1 = JSON.stringify(JSON_user);
+    const user = JSON.parse(JSON_user1);
+    console.log(user);
+    // 職種データの取得
+    const JSON_oc = <?= $result_json_occupations; ?>;
+    const JSON_oc1 = JSON.stringify(JSON_oc);
+    const oc = JSON.parse(JSON_oc1);
+    console.log(oc);
+    // 使用可能言語データの取得
+    const JSON_lang = <?= $result_json_langs; ?>;
+    const JSON_lang1 = JSON.stringify(JSON_lang);
+    const la = JSON.parse(JSON_lang1);
+    console.log(la);
   </script>
 </body>
 
