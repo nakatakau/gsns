783680ba249adf180230a8b7e9920291eb80028f
yanagawa <yanagawa0328@gmail.com>
2021-06-30 00:11:26 +0900

    新規登録、ログイン画面のデザイン完了

--- view/login.php
+++ view/login.php
@@ -1,43 +1,44 @@
 <!DOCTYPE html>
 <html lang="ja">
-
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <!-- CSSの取り込み -->
   <link rel="stylesheet" href="../css/reset.css">
-  <link rel="stylesheet" href="../css/common.css">
   <link rel="stylesheet" href="../css/login.css">
+  <link href="https://unpkg.com/sanitize.css" rel="stylesheet"/>
+  <link rel="shortcut icon" href="../img/icons/icon-48×48.png"/>
   <title>login</title>
+  <script src="https://unpkg.com/feather-icons"></script>
 </head>
-
 <body>
-  <main>
-    <!-- ログインコンテンツ -->
-    <div class="login">
-      <!-- ロゴの配置 -->
-      <div class="login_logo">
-        <img src="" alt="">ロゴ
-      </div>
-      <!-- ログイン入力エリア -->
-      <div class="input_box">
-        <form action="../app/login_act.php" method="POST" class="input_form">
-          <p class="login_text">メールアドレス</p>
-          <input type="email" name="email">
-          <p class="login_text">パスワード</p>
-          <input type="password" name="password" class="margin_bottom_add">
-          <div class="submit_btn">
-            <input type="submit" value="ログイン">
-          </div>
-        </form>
-        <!-- 新規会員登録画面へリンク -->
-        <div class="link">
-          <a href="member_registration.php">新規会員登録はこちら</a>
-        </div>
-      </div>
-    </div>
-  </main>
-</body>
 
+  <div class="title">
+    <img class="img_head" src="../img/id.svg" alt="">
+    <p class="tl_2">For</p>
+    <p class="tl_3">G Talk</p>
+  </div>
+
+  <div class="content">
+    <h1 class="lg_ttl">ログイン画面</h1>
+
+    <form action="../app/login_act.php" method="POST" class="input_form">
+    <label for="userid" class="sr-only">メールアドレス</label>
+    <input type="email" class="bottom" id="userid" name="email" required autofocus style="margin-bottom:30px;">
+
+    <label for="password" class="sr-only">パスワード</label>
+    <input type="password" class="bottom" id="password" name="password" required style="margin-bottom:30px;">
+
+    <div class="control">
+    </div>
+    <input class="btn" type="submit" id="login" value="ログイン">
+
+    <div class="shinki_tag">
+      <a class="shinki" href="../view/member_registration.php">新規登録はこちらから</a>
+    </div>
+
+  </div>
+
+</body>
 </html>
