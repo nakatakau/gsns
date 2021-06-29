e8df04305ae7e20e21dc64669adb85670744c37d
yanagawa <yanagawa0328@gmail.com>
2021-06-29 23:26:14 +0900

    デザインCSS一式一旦あげます

--- parts/footer.php
+++ parts/footer.php
@@ -1,12 +1,19 @@
 <footer>
-  <div class="display">
-    <h1 class="banner">G talk</h1>
-    <ul class="sub">
-      <a href="#">利用規約</a>
-      <a href="#">プライバシーポリシー</a>
-      <a href="#">お問い合わせ</a>
-    </ul>
+
+<div class="footer_main">
+  <div class="title">
+    <img class="img_head" src="../img/id.svg" alt="">
+    <h1 class="tl_2">For</h1>
+    <h1 class="tl_3">G Talk</h1>
   </div>
+
+  <div class="footer_masse">
+    <img class="team_logo" src="../img/teams.svg" alt="">
+    <p class="footer_lite">Produced by G's ACADEMY Tokyo LAB 11th<br>
+    Nakataku & Wattan & Maari & Yanapi of Team Development</p>
+  </div>
+</div>
+
 </footer>
 <!-- JavaScript Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
