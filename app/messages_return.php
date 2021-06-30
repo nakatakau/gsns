e900c3378da5f189913d99552e4fe15041a03b5c
masataka watada <na7esan@gmail.com>
2021-07-01 06:03:20 +0900

    コメント追加。不要な改行削除

--- app/messages_return.php
+++ app/messages_return.php
@@ -3,7 +3,6 @@ session_start();
 include_once __DIR__ . "/../app/funcs.php";
 sschk();
 $my_id = $_SESSION["my_id"];
-
 // v($my_id);
 
 //POSTデータ取得
@@ -11,10 +10,9 @@ $destination_user_id = isset($_GET['destination_user_id']) ? $_GET['destination_
 // 下記は本番環境ではコメントアウト
 // $destination_user_id=2;
 
-
 // DB接続
 $pdo = db_conn();
-
+// 自分と送信先のメッセのやりとりを一覧で取得のためのクエリ作成
 $sql = "
   SELECT
     *
@@ -30,17 +28,14 @@ $sql = "
     destination_user_id=$my_id)
   ORDER BY
     created_at ASC;
-
 ";
-
 // v($sql);
-
 
 $stmt = $pdo->prepare($sql);
 // $stmt->bindValue(':my_id', $my_id, PDO::PARAM_STR);
 $status = $stmt->execute();
 
-//SQL実行時にエラーがある場合STOP
+//クエリ実行時にエラーがある場合に停止
 if ($status == false) {
   sql_error($stmt);
 }
@@ -51,5 +46,5 @@ $destination_users = $stmt->fetchAll(PDO::FETCH_ASSOC);
 $result_json_destination_users = json_encode($destination_users);
 // v($result_json_destination_users);
 
-
+// フロント側への返却処理
 echo $result_json_destination_users;
