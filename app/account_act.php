10698232e840c892201330f613c768d60cb80fcd
masataka watada <na7esan@gmail.com>
2021-06-30 20:48:30 +0900

    accountページで既存のデータが表示されない、登録できないバグを修正

--- app/account_act.php
+++ app/account_act.php
@@ -1,41 +1,47 @@
 <?php
-
+session_start();
 include_once __DIR__ . "/funcs.php";
+
+//SESSIONからmy_idを取得
+$my_id = $_SESSION["my_id"];
+// v($my_id);
 
 //1. POSTデータ取得
 $mail  = $_POST["email"];
 $pass  = $_POST["password"];
 $name  = $_POST["name"];
-$my_id = $_POST["my_id"];
-// v($name);
 // v($mail);
 // v($pass);
+// v($name);
 
 //本来はここでバリデーションを入れるべき
 
 
 //2. DB接続します
-//function内の$pdoを受け取る
 $pdo = db_conn();
 
 
-if($pass==""){
-    $sql = "UPDATE users SET mail=:mail,name=:name WHERE my_id=:my_id";
+if($pass===""){
+  // echo 'passは更新されません';
+    $sql = "UPDATE users SET mail=:mail,name=:name WHERE user_id=:my_id";
 }else{
-    $sql = "UPDATE users SET mail=:mail,name=:name,pass=:pass WHERE my_id=:my_id";
+  // echo 'passは更新されましたか？';
+    $sql = "UPDATE users SET mail=:mail,name=:name,pass=:pass WHERE user_id=:my_id";
 }
+v($sql);
 
 $stmt = $pdo->prepare($sql);
 
 //Bind変数へ代入
-if($pass!=""){
-$stmt->bindValue(':pass', password_hash($pass, PASSWORD_DEFAULT), PDO::PARAM_STR);
+if($pass!==""){
+$stmt->bindValue(':pass', $pass, PDO::PARAM_STR);
 }
 
 //３．データ登録SQL作成
 $stmt->bindValue(':mail', $mail, PDO::PARAM_STR);    //Integer（数値の場合 PDO::PARAM_INT)
-$stmt->bindValue(':pass', $pass, PDO::PARAM_STR);    //Integer（数値の場合 PDO::PARAM_INT)
+// $stmt->bindValue(':pass', $pass, PDO::PARAM_STR);    //Integer（数値の場合 PDO::PARAM_INT)
 $stmt->bindValue(':name', $name, PDO::PARAM_STR);    //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':my_id', $my_id, PDO::PARAM_INT);    //Integer（数値の場合 PDO::PARAM_INT)
 $status = $stmt->execute();
 
 //４．データ登録処理後
--- view/account.php
+++ view/account.php
@@ -4,12 +4,20 @@ include_once __DIR__ . "/../app/funcs.php";
 
 //SESSIONからmy_idを取得
 $my_id = $_SESSION["my_id"];
+// v($my_id);
 
 //DB接続します
 $pdo = db_conn();
 
 //ユーザデータ取得のためのSQL作成
-$sql = "SELECT * FROM users WHERE user_id=:my_id";
+$sql = "
+  SELECT
+    mail,pass,name
+  FROM
+    users
+  WHERE
+    user_id=:my_id;
+";
 $stmt = $pdo->prepare($sql);
 $stmt->bindValue(':my_id', $my_id, PDO::PARAM_STR);
 $status = $stmt->execute();
@@ -18,10 +26,15 @@ $status = $stmt->execute();
 if ($status == false) {
   sql_error($stmt);
 }
+
+// my_idで絞り込んだ一件分のデータを取得
+$user_data = $stmt->fetch(PDO::FETCH_ASSOC);
+// v($user_data);
 ?>
 
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -34,51 +47,52 @@ if ($status == false) {
   <link rel="stylesheet" href="../css/common.css">
   <!-- ページ内のCSS読み込み -->
   <link rel="stylesheet" href="../css/account.css">
-    <!-- line-awesome -->
+  <!-- line-awesome -->
   <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
   <!-- アイコン設定 -->
-  <link rel="shortcut icon" href="../icon/icon_48.png"/>
+  <link rel="shortcut icon" href="../icon/icon_48.png" />
   <title>アカウントページ</title>
   <!-- featherアイコンの読み込み -->
   <script src="https://unpkg.com/feather-icons"></script>
 </head>
+
 <body>
-<?php
-include("../parts/header.php");
-?>
-<main>
-  <!-- 編集ページ -->
-  <div class="display">
-    <form action="account_act.php" method="post">
+  <?php
+  // include("../parts/header.php");
+  ?>
+  <main>
+    <!-- 編集ページ -->
+    <div class="display">
+      <form action="../app/account_act.php" method="post">
 
-    <div class="form">
-      <input id="" value="<?php echo $hoge["email"]?>" class="form_line" type="email" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$">
+        <div class="form">
+          <input id="" value="<?php echo $user_data['mail']; ?>" class="form_line" type="email" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$">
+        </div>
+
+        <div class="form">
+          <input id="" class="form_line" type="password" name="password" pattern="^([a-zA-Z0-9]{6,})$" placeholder="パスワード">
+        </div>
+        <font class="fontsize" color="#0000ff">半角英数字6文字以上で入力ください。</font>
+
+        <div class="form">
+          <input id="" value="<?php echo $user_data['name']; ?>" class="form_line " type="text" name="name">
+        </div>
+
+
+        <input type="hidden" name="id" value="<?php echo $my_id ?>">
+
+        <button type="btn" class="btn_edit">更新</button>
+      </form>
     </div>
 
-    <div class="form">
-      <input id="" class="form_line" type="password" name="password" pattern="^([a-zA-Z0-9]{6,})$" placeholder="パスワード">
-    </div>
-    <font class="fontsize" color="#0000ff">半角英数字6文字以上で入力ください。</font>
-
-    <div class="form">
-      <input id="" value="<?php echo $hoge["name"]?>" class="form_line " type="text" name="name" value="">
-    </div>
-
-    <input type="hidden" name="id" value="<?php echo $my_id ?>">
-
-    <button type="btn" class="btn_edit" >更新</button>
-    </form>
-  </div>
-
-</main>
-<?php
-include("../parts/footer.php");
-?>
-<script>
-
-//featherアイコン
-feather.replace();
-
-</script>
+  </main>
+  <?php
+  include("../parts/footer.php");
+  ?>
+  <script>
+    //featherアイコン
+    feather.replace();
+  </script>
 </body>
+
 </html>
